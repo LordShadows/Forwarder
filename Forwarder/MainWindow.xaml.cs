fbf3a8fd86e953ad67a968e0217e8da319572e5a
Даниил Сандруцкий <house.ru@yandex.by>
2017-06-14 21:30:36 +0300

    Сделана основа интерфейс

--- Forwarder/MainWindow.xaml.cs
+++ Forwarder/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Forwarder
@@ -20,9 +12,211 @@ namespace Forwarder
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isRightResize = false;
+        //bool isLeftResize = false;
+        bool isBottomResize = false;
+        bool isRightBottomResize = false;
+        double positionRightResize = 0;
+        //double positionLeftResize = 0;
+        double positionBottomResize = 0;
+        double positionXRightBottomResize = 0;
+        double positionYRightBottomResize = 0;
+
+        const double minWidth = 100;
+        const double minHight = 100;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Close_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void HeaderButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            close.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\close-hover.png", UriKind.Relative)));
+            min.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\min-hover.png", UriKind.Relative)));
+            max.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\max-hover.png", UriKind.Relative)));
+        }
+
+        private void HeaderButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            close.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\close-normal.png", UriKind.Relative)));
+            min.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\min-normal.png", UriKind.Relative)));
+            max.Fill = new ImageBrush(new BitmapImage(new Uri(@"Resources\max-normal.png", UriKind.Relative)));
+        }
+
+        private void Max_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(Properties.Settings.Default.Maximized)
+            {
+                this.Height = Properties.Settings.Default.Height;
+                this.Width = Properties.Settings.Default.Width;
+                this.Top = Properties.Settings.Default.Top;
+                this.Left = Properties.Settings.Default.Left;
+                Properties.Settings.Default.Maximized = false;
+                Properties.Settings.Default.Save();
+                this.body.Margin = new Thickness(20.0, 20.0, 20.0, 20.0);
+            }
+            else
+            {
+                Properties.Settings.Default.Height = this.Height;
+                Properties.Settings.Default.Width = this.Width;
+                Properties.Settings.Default.Top = this.Top;
+                Properties.Settings.Default.Left = this.Left;
+                Properties.Settings.Default.Maximized = true;
+                Properties.Settings.Default.Save();
+
+                this.Height = SystemParameters.WorkArea.Height;
+                this.Width = SystemParameters.WorkArea.Width;
+                this.Top = SystemParameters.WorkArea.Top;
+                this.Left = SystemParameters.WorkArea.Left;
+                this.body.Margin = new Thickness(0.0, 0.0, 0.0, 0.0);
+            }
+            
+        }
+
+        private void Min_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(!Properties.Settings.Default.Maximized) this.DragMove();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                Properties.Settings.Default.Height = this.Height;
+                Properties.Settings.Default.Width = this.Width;
+                Properties.Settings.Default.Top = this.Top;
+                Properties.Settings.Default.Left = this.Left;
+                Properties.Settings.Default.Maximized = true;
+                Properties.Settings.Default.Save();
+
+                this.Height = SystemParameters.WorkArea.Height;
+                this.Width = SystemParameters.WorkArea.Width;
+                this.Top = SystemParameters.WorkArea.Top;
+                this.Left = SystemParameters.WorkArea.Left;
+                this.body.Margin = new Thickness(0.0, 0.0, 0.0, 0.0);
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        #region Resize Window
+
+        private void RightResize_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isRightResize && !Properties.Settings.Default.Maximized)
+            {
+                double newWidth = this.Width + (e.GetPosition(this).X - positionRightResize);
+                if (newWidth > minWidth + 40) {
+                    this.Width = newWidth;
+                    positionRightResize = e.GetPosition(this).X;
+                }
+            }
+        }
+
+        private void RightBottomResize_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isRightBottomResize && !Properties.Settings.Default.Maximized)
+            {
+                double newWidth = this.Width + (e.GetPosition(this).X - positionXRightBottomResize);
+                double newHeight = this.Height + (e.GetPosition(this).Y - positionYRightBottomResize);
+                if (newWidth > minWidth + 40)
+                {
+                    this.Width = newWidth;
+                    positionXRightBottomResize = e.GetPosition(this).X;
+                }
+                if (newHeight > minHight + 40)
+                {
+                    this.Height = newHeight;
+                    positionYRightBottomResize = e.GetPosition(this).Y;
+                }
+            }
+        }
+
+        /*private void LeftResize_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isLeftResize && !Properties.Settings.Default.Maximized)
+            {
+                double newWidth = this.Width - (e.GetPosition(this).X - positionLeftResize);
+                double newLeft = this.Left + (e.GetPosition(this).X - positionLeftResize);
+                if (newWidth > minWidth + 40)
+                {
+                    this.Width = newWidth;
+                    this.Left = newLeft;
+                }
+            }
+        }*/
+
+        private void BottomResize_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isBottomResize && !Properties.Settings.Default.Maximized)
+            {
+                double newHeight = this.Height + (e.GetPosition(this).Y - positionBottomResize);
+                if (newHeight > minHight + 40)
+                {
+                    this.Height = newHeight;
+                    positionBottomResize = e.GetPosition(this).Y;
+                }
+            }
+        }
+
+        private void Resize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.CaptureMouse();
+            switch (rect.Name)
+            {
+                case "rightResize":
+                    isRightResize = true;
+                    positionRightResize = e.GetPosition(this).X;
+                    break;
+                /*case "leftResize":
+                    isLeftResize = true;
+                    positionLeftResize = e.GetPosition(this).X;
+                    break;*/
+                case "bottomResize":
+                    isBottomResize = true;
+                    positionBottomResize = e.GetPosition(this).Y;
+                    break;
+                case "rightBottomResize":
+                    isRightBottomResize = true;
+                    positionXRightBottomResize = e.GetPosition(this).X;
+                    positionYRightBottomResize = e.GetPosition(this).Y;
+                    break;
+            } 
+        }
+
+        private void Resize_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.ReleaseMouseCapture();
+            switch (rect.Name)
+            {
+                case "rightResize":
+                    isRightResize = false;
+                    break;
+                /*case "leftResize":
+                    isLeftResize = false;
+                    break;*/
+                case "bottomResize":
+                    isBottomResize = false;
+                    break;
+                case "rightBottomResize":
+                    isRightBottomResize = false;
+                    break;
+            }
+        }
+
+        #endregion
+   
     }
 }
